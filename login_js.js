378ac48757a80d4a80a2f8aeac078e91f1bac421
etang <etang315@gmail.com>
2016-04-03 14:39:05 -0500

    added if else statement to check for input field are empty. Added else statemnet if login successful, for not is empty. Planning to have upon login successful directed to the next page and display the correct data upon the user login student status.

--- login_js.js
+++ login_js.js
@@ -5,5 +5,19 @@ but.addEventListener('click',function login(){
     var user = document.forms["iit"]["user"].value;
     var pass = document.forms["iit"}["pass"].value;
     
-
+    if ((user == null || user =="") && (pass == null || pass ="")){
+        alert("Please do not leave the fields empty"); //will change it later to color field input 
+    }
+    
+    if else ((user == null) || user == ""){
+        alert("Please do not leave the user field input field empty")
+    }
+    
+    if else((pass == null) || (pass == "")){
+        alert("Please enter your password")
+    }
+    
+    else ((user == "iit_test") && (pass == "password")){
+        //work on this later.
+    }
                      });

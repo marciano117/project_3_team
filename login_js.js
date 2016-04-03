b2cd9ee4667134c93adca203f75affefba5af54a
etang <etang315@gmail.com>
2016-04-03 12:58:18 -0500

    added loginjs.js, working on the condition for checking login statement. Basic javascript. Added event handled that runs when user click login button. Added two var that takes in the value from the input field. Will work on the condition statement to check for empty field later.

---
+++ login_js.js
@@ -0,0 +1,9 @@
+var but = document.getElementById("login");
+
+but.addEventListener('click',function login(){
+                     
+    var user = document.forms["iit"]["user"].value;
+    var pass = document.forms["iit"}["pass"].value;
+    
+
+                     });

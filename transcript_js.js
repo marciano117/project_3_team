049222c89ef60edfb0337c4c95253768e4f88b6e
etang <etang315@gmail.com>
2016-04-11 14:57:36 -0500

    script an event listener when the user click on submit button to send in the request for the transcript. It will change the class name of the clear-display to display to show the message that the offical transcript request has been sent.

---
+++ transcript_js.js
@@ -0,0 +1,6 @@
+var but1 = document.getElementById("submit");
+
+but1.addEventListener('click',function request(){
+    var msg = document.getElementById("transcript_message");
+    msg.className = "display";
+});

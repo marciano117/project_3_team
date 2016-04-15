46078e204b6d114f01df40c079b0002026b16c45
etango <etang315@gmail.com>
2016-04-15 17:54:22 -0500

    added more javascript for conditional statement of input field from transcript

--- transcript_js.js
+++ transcript_js.js
@@ -19,21 +19,63 @@ but1.addEventListener('click',function request(){
     var email_id = document.getElementById("email");
     var msg = document.getElementById("transcript_message");
     var error_msg = document.getElementById("error");
+    var err_1 = document.getElementById("err_1");
+    var err_2 = document.getElementById("err_2");
+    var err_both = document.getElementById("err_both");
+    var err_payment = document.getElementById("err_payment");
+    var err_shipping = document.getElementById("err_shipping");
     
     var value_array = [user1,name,birth,address,email];
     var id_array = [user_id,name_id,birth_id,address_id,email_id,payment_id,delivery_id];
     var i=0;
     
-    if(user1 == null || user1 == "" || name == null || name == "" || birth == null || birth == "" || address == null || address == "" || delivery == null || delivery == "" || payment == null || payment == "" || email == null || email ==""){
+    
+    if(user1 == null || user1 == "" || name == null || name == "" || birth == null || birth == "" || address == null || address == "" || delivery == "Select Delivery Option" || payment == "Select Payment Option" || email == null || email ==""){
          
+        msg.className = "clear";
+        error_msg.className="clear";
+        err_1.className = "clear";
+        err_2.className = "clear";
+        err_both.className = "clear";
+        err_payment.className = "clear";
+        err_shipping.className = "clear";
+        
         for(i=0;i<value_array.length;i++){
             if(value_array[i] == null || value_array[i] ==""){
                 id_array[i].className = "red";
+                if(delivery == "Select Delivery Option" && payment == "Select Payment Option"){
+                    error_msg.className = "display";
+                }
             }  
         }
+        if(delivery == "Select Delivery Option" && payment == "Select Payment Option" && value_array != null & value_array !="" && error_msg.className == "clear"){
+            err_both.className = "display";
+        }
+        else if(delivery !="Select Delivery Option" && payment == "Select Payment Option" && value_array != null & value_array !="" && error_msg.className == "clear"){
+            err_payment.className = "display";
+        }
+        
+        else if(delivery =="Select Delivery Option" && payment != "Select Payment Option" && value_array != null & value_array !="" && error_msg.className == "clear"){
+            err_shipping.className = "display";
+        }
+        else if(delivery !="Select Delivery Option" && payment == "Select Payment Option" && value_array == null && error_msg.className == "clear"){
+            err_1.className = "display";
+        }
+        
+        else if(delivery =="Select Delivery Option" && payment != "Select Payment Option" && value_array == null && error_msg.className == "clear"){
+            err_2.className = "display";
+        }
     }
+   
+
     else {
         
+        error_msg.className="clear";
+        err_1.className = "clear";
+        err_2.className = "clear";
+        err_both.className = "clear";
+        err_payment.className = "clear";
+        err_shipping.className = "clear";
         msg.className = "display";
         user_id.value = "";
         name_id.value = "";

29a97c1051c06ab1d266336cca6af8ec6c06e49f
etango <etang315@gmail.com>
2016-04-15 16:51:44 -0500

    Added an array that stores the value and id so when running through a for loop it will check for input field if it's empty or not. If it's empty it will change the background to red to indicate the user to field out empty field. Also added value clear upon successful submit of form

--- transcript_js.js
+++ transcript_js.js
@@ -6,23 +6,43 @@ but1.addEventListener('click',function request(){
     var name = document.forms["request"]["name"].value;
     var birth = document.forms["request"]["birth"].value;
     var address = document.forms["request"]["address"].value;
-    
+    var delivery = document.forms["request"]["Shipping"].value;
+    var payment = document.forms["request"]["Payment"].value
+    var email = document.forms["request"]["email"].value;
     
     var user_id = document.getElementById("user1");
     var name_id = document.getElementById("name");
     var birth_id = document.getElementById("birth");
     var address_id = document.getElementById("address");
+    var delivery_id = document.getElementById("shipping");
+    var payment_id = document.getElementById("payment");
+    var email_id = document.getElementById("email");
     var msg = document.getElementById("transcript_message");
+    var error_msg = document.getElementById("error");
     
-    if(user1 == null || user1 == "" || name == null || name == "" || birth == null || birth == "" || address == null || address == ""){
-        
-        msg.className = "clear-display";
-        
+    var value_array = [user1,name,birth,address,email];
+    var id_array = [user_id,name_id,birth_id,address_id,email_id,payment_id,delivery_id];
+    var i=0;
+    
+    if(user1 == null || user1 == "" || name == null || name == "" || birth == null || birth == "" || address == null || address == "" || delivery == null || delivery == "" || payment == null || payment == "" || email == null || email ==""){
+         
+        for(i=0;i<value_array.length;i++){
+            if(value_array[i] == null || value_array[i] ==""){
+                id_array[i].className = "red";
+            }  
+        }
     }
     else {
         
         msg.className = "display";
-        
+        user_id.value = "";
+        name_id.value = "";
+        birth_id.value = "";
+        address_id.value = "";
+        email_id.value = "";
+        delivery_id.selectedIndex=0;
+        payment_id.selectIndex=0;
     }
+    
     
 });
